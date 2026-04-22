4195f347289b81cfc914330e65b59a1ef36ddbad
marcotristani <marco.tristani@hotmail.it>
2026-04-22 15:07:05 +0200

    create list per canzoni e film e mandate alle pagine corrispondenti

--- src/main/java/org/java/spring/best_of_the_year/controllers/MoviesController.java
+++ src/main/java/org/java/spring/best_of_the_year/controllers/MoviesController.java
@@ -1,5 +1,7 @@
 package org.java.spring.best_of_the_year.controllers;
 
+import java.util.ArrayList;
+
 import org.java.spring.best_of_the_year.classes.Movie;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,12 +12,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequestMapping("/")
 public class MoviesController {
 
+    private ArrayList<Movie> getBestMovies(ArrayList<Movie> movies) {
+
+        ArrayList<Movie> bestMovies = new ArrayList<>();
+        for (Movie movie : movies) {
+            if (movie.getAverageVote() > 4f) {
+                bestMovies.add(movie);
+            }
+        }
+        return bestMovies;
+    }
+
     @GetMapping("/movies")
     public String movies(Model model) {
 
-        Movie film1 = new Movie(1, "la bella e la bestia", 4.5f);
+        // creo array dove andare a mettere i film
+        ArrayList<Movie> movies = new ArrayList<>();
 
-        model.addAttribute("movie", film1);
+        // creo i film
+        Movie film1 = new Movie(1, "la bella e la bestia", 4.5f);
+        Movie film2 = new Movie(2, "principessa kitty", 2.5f);
+        Movie film3 = new Movie(3, "aladino", 5f);
+
+        movies.add(film1);
+        movies.add(film2);
+        movies.add(film3);
+
+        model.addAttribute("movies", getBestMovies(movies));
 
         return "movies";
     }
--- src/main/java/org/java/spring/best_of_the_year/controllers/SongsController.java
+++ src/main/java/org/java/spring/best_of_the_year/controllers/SongsController.java
@@ -1,5 +1,6 @@
 package org.java.spring.best_of_the_year.controllers;
 
+import java.util.ArrayList;
 import org.java.spring.best_of_the_year.classes.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,12 +11,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequestMapping("/")
 public class SongsController {
 
+    private ArrayList<Song> getBestSongs(ArrayList<Song> songs) {
+
+        ArrayList<Song> bestSongs = new ArrayList<>();
+        for (Song song : songs) {
+            if (song.getAverageVote() > 4f) {
+                bestSongs.add(song);
+            }
+        }
+        return bestSongs;
+    }
+
     @GetMapping("/songs")
-    public String song(Model model) {
+    public String songs(Model model) {
+
+        ArrayList<Song> songs = new ArrayList<>();
 
         Song canzone1 = new Song(1, "la canzone del sole", 5.00f);
+        Song canzone2 = new Song(2, "Viva la pappa", 3.00f);
+        Song canzone3 = new Song(3, "Brividi", 4.2f);
 
-        model.addAttribute("song", canzone1);
+        songs.add(canzone1);
+        songs.add(canzone2);
+        songs.add(canzone3);
+
+        model.addAttribute("songs", getBestSongs(songs));
         return "songs";
     }
 
