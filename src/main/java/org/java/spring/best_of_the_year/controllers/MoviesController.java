1981c37bec0ef338216d7d2b7fb83327f9a46a7f
marcotristani <marco.tristani@hotmail.it>
2026-04-23 10:41:48 +0200

    gestione dettaglio canzone o film e valutato caso id non esistente con pagina not found

--- src/main/java/org/java/spring/best_of_the_year/controllers/MoviesController.java
+++ src/main/java/org/java/spring/best_of_the_year/controllers/MoviesController.java
@@ -7,10 +7,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequestMapping("/")
 public class MoviesController {
+
+    // creo array dove andare a mettere i film
+    private ArrayList<Movie> movies = new ArrayList<>();
+
+    // creo i film
+    private Movie film1 = new Movie(1, "la bella e la bestia", 4.5f);
+    private Movie film2 = new Movie(2, "principessa kitty", 4.5f);
+    private Movie film3 = new Movie(3, "aladino", 5f);
+
+    {
+        movies.add(film1);
+        movies.add(film2);
+        movies.add(film3);
+    };
 
     private ArrayList<Movie> getBestMovies(ArrayList<Movie> movies) {
 
@@ -26,21 +41,26 @@ public class MoviesController {
     @GetMapping("/movies")
     public String movies(Model model) {
 
-        // creo array dove andare a mettere i film
-        ArrayList<Movie> movies = new ArrayList<>();
-
-        // creo i film
-        Movie film1 = new Movie(1, "la bella e la bestia", 4.5f);
-        Movie film2 = new Movie(2, "principessa kitty", 2.5f);
-        Movie film3 = new Movie(3, "aladino", 5f);
-
-        movies.add(film1);
-        movies.add(film2);
-        movies.add(film3);
-
-        model.addAttribute("movies", getBestMovies(movies));
+        model.addAttribute("movies", getBestMovies(this.movies));
 
         return "movies";
     }
 
+    @GetMapping("/movies/{id}")
+    public String movieDetail(Model model, @PathVariable("id") int id) {
+
+        Boolean thereIs = false;
+        for (Movie movie : movies) {
+            if (id == movie.getId()) {
+                model.addAttribute("element", movie);
+                thereIs = true;
+
+            }
+        }
+        if (thereIs) {
+            return "detail";
+        } else {
+            return "notFound";
+        }
+    }
 }
--- src/main/java/org/java/spring/best_of_the_year/controllers/SongsController.java
+++ src/main/java/org/java/spring/best_of_the_year/controllers/SongsController.java
@@ -1,15 +1,28 @@
 package org.java.spring.best_of_the_year.controllers;
 
 import java.util.ArrayList;
+
 import org.java.spring.best_of_the_year.classes.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequestMapping("/")
 public class SongsController {
+
+    ArrayList<Song> songs = new ArrayList<>();
+    Song canzone1 = new Song(1, "la canzone del sole", 5.00f);
+    Song canzone2 = new Song(2, "Viva la pappa", 3.00f);
+    Song canzone3 = new Song(3, "Brividi", 4.2f);
+
+    {
+        songs.add(canzone1);
+        songs.add(canzone2);
+        songs.add(canzone3);
+    }
 
     private ArrayList<Song> getBestSongs(ArrayList<Song> songs) {
 
@@ -25,18 +38,26 @@ public class SongsController {
     @GetMapping("/songs")
     public String songs(Model model) {
 
-        ArrayList<Song> songs = new ArrayList<>();
-
-        Song canzone1 = new Song(1, "la canzone del sole", 5.00f);
-        Song canzone2 = new Song(2, "Viva la pappa", 3.00f);
-        Song canzone3 = new Song(3, "Brividi", 4.2f);
-
-        songs.add(canzone1);
-        songs.add(canzone2);
-        songs.add(canzone3);
-
         model.addAttribute("songs", getBestSongs(songs));
         return "songs";
+    }
+
+    @GetMapping("/songs/{id}")
+    public String movieDetail(Model model, @PathVariable("id") int id) {
+
+        Boolean thereIs = false;
+        for (Song song : songs) {
+            if (id == song.getId()) {
+                model.addAttribute("element", song);
+                thereIs = true;
+            }
+        }
+        if (thereIs) {
+            return "detail";
+        } else {
+            return "notFound";
+        }
+
     }
 
 }
